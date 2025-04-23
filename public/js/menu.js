18352cbf09d85b5afc07cf5bb37858e9d87f1222
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-04-23 13:46:52 +0100

    Rework header

---
+++ public/js/menu.js
@@ -0,0 +1,21 @@
+const toggler = document.getElementById("toggler");
+const dropdown = document.getElementById("dropdown");
+const header = document.querySelector("header");
+
+toggler.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdown.classList.toggle('active');
+
+    if (dropdown.classList.contains("active")){
+        header.style.backgroundColor = "rgb(26, 26, 26)";
+    } else {
+        header.style.backgroundColor = "";
+    }
+});
+
+document.addEventListener("click", () => {
+    if (dropdown){
+        dropdown.classList.remove("active");
+        header.style.backgroundColor = "";
+    }
+});
