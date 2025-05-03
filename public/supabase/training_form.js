2108bd4a54d260f8759ee5370259f213b44e0ffd
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-05-03 17:01:15 +0100

    update fetch logic for all dropdown

--- public/supabase/training_form.js
+++ public/supabase/training_form.js
@@ -4,6 +4,9 @@ console.log(supabaseClient);
 const trainingForm = document.getElementById("training-form");
 const qualificationDropDown = document.getElementById("qualification");
 const countryDropDown = document.getElementById("country");
+const programDropDown = document.getElementById("program");
+const referralDropDown = document.getElementById("referral");
+const userTypeDropDown = document.getElementById("user-type");
 
 document.addEventListener("DOMContentLoaded", async () => {
     // Fetch qualifications
@@ -37,4 +40,66 @@ document.addEventListener("DOMContentLoaded", async () => {
             countryDropDown.appendChild(option);
         });
     }
+
+    // Fetch programs
+    const { data: programData, error: programError } = await supabaseClient
+        .from('program')
+        .select("*");
+
+    if (programError) {
+        console.log(programError);
+    } else {
+        programData.forEach((item) => {
+            const option = document.createElement("option");
+            option.value = item.id;
+            option.textContent = item.programs;
+            programDropDown.appendChild(option);
+        });
+    }
+
+    // Fetch referrals
+    const { data: referralData, error: referralError } = await supabaseClient
+        .from('referral_source')
+        .select("*");
+
+    if (referralError) {
+        console.log(referralError);
+    } else {
+        referralData.forEach((item) => {
+            const option = document.createElement("option");
+            option.value = item.id;
+            option.textContent = item.referral_source;
+            referralDropDown.appendChild(option);
+        });
+    }
+
+    // Fetch user-type
+    const { data: userTypeData, error: userTypeError } = await supabaseClient
+        .from('user_type')
+        .select("*");
+
+    if (userTypeError) {
+        console.log(userTypeError);
+    } else {
+        userTypeData.forEach((item) => {
+            const option = document.createElement("option");
+            option.value = item.id;
+            option.textContent = item.user_type;
+            userTypeDropDown.appendChild(option);
+        });
+    }
+
+    trainingForm.addEventListener("submit", async () => {
+
+        const formData = {
+            name: trainingForm.name.value,
+            email: trainingForm.email.value,
+            course: trainingForm.course.value,
+          };
+          console.log(formData);
+
+        const { error } = await supabase
+        .from('countries')
+        .insert({ id: 1, name: 'Mordor' })
+    })
 });
