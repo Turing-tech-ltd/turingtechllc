4e5d41d862345c2ea765078a5276315101e920df
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-11-06 16:13:13 +0100

    Complete all changes

--- public/js/socials.js
+++ public/js/socials.js
@@ -2,6 +2,7 @@ const instagramButtons = document.querySelectorAll(".instagram");
 const linkedinButtons = document.querySelectorAll(".linkedin");
 const tiktokButtons = document.querySelectorAll(".tiktok");
 const facebookButtons = document.querySelectorAll(".facebook");
+const twitterButtons = document.querySelectorAll(".twitter");
 
 instagramButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
@@ -11,18 +12,24 @@ instagramButtons.forEach((btn) => {
 
 linkedinButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        window.open("http://linkedin.com/company/turing-tech-hub", "_blank");
+        window.open("https://www.linkedin.com/company/turingtechllc", "_blank");
     });
 });
 
 facebookButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        window.open("https://web.facebook.com/profile.php?id=61565730746371", "_blank");
+        window.open("https://web.facebook.com/turingtechacademy/", "_blank");
     });
 });
 
 tiktokButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        window.open("https://www.tiktok.com/@turing_tech", "_blank");
+        window.open("https://www.tiktok.com/@turingtechllc", "_blank");
+    });
+});
+
+twitterButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        window.open("https://x.com/turingtechllc", "_blank");
     });
 });
